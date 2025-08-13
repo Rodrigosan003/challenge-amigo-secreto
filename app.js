7c58d226678a428980d84b4a97cc2a53b3b0f2cf
Rodrigosan003 <r.santanaa003@gmail.com>
2025-08-13 10:39:54 -0300

    funções criadas

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+function adicionarAmigo() {
+  //   alert("Amigo adicionado com sucesso.");
+  const amigo = document.getElementById("amigo").value;
+  let amigos = [];
+  amigos.push(amigo);
+  console.log(amigos);
+}
+
+function sortearAmigo() {
+  // Lógica para sortear um amigo
+  alert("Sorteio realizado com sucesso.");
+}
